f0d84caa0310bd502f70005dd6bb990215d780f4
Mike GZ <m.galvez@tec.mx>
2024-06-10 17:24:41 +0000

    V10624.1124

--- web_interface/launch/tf2_web_republisher_launch.py
+++ web_interface/launch/tf2_web_republisher_launch.py
@@ -1,36 +1,31 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-import os
 
 def generate_launch_description():
-    # Define the path to the URDF file
-    urdf_file = os.path.join(
-        FindPackageShare('ur_description').find('ur_description'),
-        'urdf',
-        'ur.urdf.xacro'
-    )
+
 
     return LaunchDescription([
         Node(
             package='tf2_web_republisher_py',
             executable='tf2_web_republisher',
             name='tf2_web_republisher',
-            output='screen',
-            parameters=[{'use_sim_time': True}]
+            output='screen'
         ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': True}],
-            arguments=[urdf_file]
-        ),
+ 
         Node(
             package='joint_state_publisher',
             executable='joint_state_publisher',
+            name='joint_state_publisher',
             output='screen',
             parameters=[{'use_sim_time': True}]
-        )
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen',
+            parameters=[{'use_sim_time': True}]
+        ),
     ])
